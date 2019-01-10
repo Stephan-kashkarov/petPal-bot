701bb8ae38f20acb0b09833ec33a52063bf03957
unknown <skas6279@WGC63CD2.internal.usyd.edu.au>
2019-01-10 11:13:33 +1100

    added route

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,10 @@
 from app import app
+from flask import request
 
 @app.route("/")
 def index():
     return 'Hello!'
+
+@app.route("/hello/<msg>", methods=["GET", 'POST'])
+def hello(msg):
+    return msg
